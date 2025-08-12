e7427ca42f69786b1249cfdb3d9f0b77d971cc11
joshuafouch <joshfouch777@gmail.com>
2025-08-12 12:15:13 -0800

    adding flowbite svelte for components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Hero from '$lib/components/Hero.svelte';
   import Navbar from '$lib/components/Navbar.svelte';
+  import { Alert } from 'flowbite-svelte';
 </script>
 
 <div class="min-h-screen flex flex-col bg-base-100">
@@ -11,4 +12,11 @@
   <main class="flex-grow flex items-center justify-center">
     <Hero />
   </main>
+</div>
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
 </div>
