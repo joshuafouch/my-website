0a1b12346d5af762fc347208258b84276588b94f
joshuafouch <joshfouch777@gmail.com>
2025-08-15 10:53:14 -0800

    config

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = 'Joshua Fouch'
+export const description = 'Joshua\'s Digital Garden'
+export const url = dev ? 'http://localhost:5173/' : 'https://joshuafouch.vercel.app/'
