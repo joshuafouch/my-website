36f8380a93183f9fcdf216e37848eefc321ebe5a
joshuafouch <joshfouch777@gmail.com>
2025-08-14 20:00:48 -0800

    cool respond button

---
+++ src/lib/components/RespondBtn.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+
+	let response = false;
+
+	function toggleResponse() {
+		response = !response
+	}
+
+</script>
+
+{#if response}
+  <blockquote transition:fade={{delay: 300, duration: 350}} class="text-xl font-title pt-5"> 
+		Glory to Him forever.
+  </blockquote>
+{/if}
+
+<button class="btn btn-primary" on:click={toggleResponse}><span class="font-main">Respond</span></button>
+
