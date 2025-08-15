090d32e20d914799e89927773c2c25829510e381
joshuafouch <joshfouch777@gmail.com>
2025-08-15 10:53:24 -0800

    navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,21 +1,46 @@
 <script>
-	const navLinks = [
-		{ href: '/about', text: 'About' },
-		{ href: '/projects', text: 'Projects' },
-		{ href: '/blog', text: 'Devblog' },
-		{ href: '/contact', text: 'Contact' }
-	];
+  let hovering = false;
+
+  const navLinks = [
+    { href: '/about', text: 'About' },
+    { href: '/projects', text: 'Projects' },
+    { href: '/blog', text: 'Devblog' },
+    { href: '/contact', text: 'Contact' }
+  ];
 </script>
 
-<div class="navbar bg-base-100 shadow-sm">
+svelte:<svelte:head>
+  <link rel="preload" href="/assets/JF_navlogo_light.webp" as="image" type="image/webp">
+  <link rel="preload" href="/assets/JF_navlogo_green.webp" as="image" type="image/webp">
+</svelte:head>
+
+<div class="navbar bg-base-100 shadow-sm font-title fixed top-0 z-50 w-full">
   <div class="flex-1">
-    <a class="btn btn-ghost text-2xl italic" href="/">Joshua Fouch</a>
+    <a 
+      href="/"
+      on:mouseenter={() => hovering = true}
+      on:mouseleave={() => hovering = false}
+    >
+      {#if !hovering}
+	<img 
+	  src="/assets/JF_navlogo_light.webp" 
+	  alt="Logo white"
+	  class="transition ease-in-out"
+	>
+      {:else}
+	<img 
+	  src="/assets/JF_navlogo_green.webp" 
+	  alt="Logo green"
+	  class="transition ease-in-out"
+	>
+      {/if}
+    </a>
   </div>
-  <!--<div class="flex-none">
+  <div class="flex-none">
     <ul class="menu menu-horizontal px-1 text-xl">
-			<!--{#each navLinks as link (link.href)}
-				<li><a href={link.href}>e</a></li>
-			{/each}
+      {#each navLinks as link (link.href)}
+	<li class="hover:text-success"><a href={link.href}>{link.text}</a></li>
+      {/each}
     </ul>
-  </div>-->
+  </div>
 </div>
