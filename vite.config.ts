8f2134608d0b9d9bfaaff618bd825ca0c1b23c42
joshuafouch <joshfouch777@gmail.com>
2025-08-31 14:01:08 -0400

    new port

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 7777, // Change the default port (e.g., 5173)
+  },
   plugins: [tailwindcss(), sveltekit()]
 });
