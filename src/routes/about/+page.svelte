d061303ecc0d701e2656b2c2a5f88dd24b37f13c
joshuafouch <joshfouch777@gmail.com>
2025-09-03 09:30:45 -0400

    began the about page, lookin good

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,74 @@
+<svelte:head>
+  <title>About Me</title>
+  <meta
+    name="description"
+    content="hi"
+  />
+</svelte:head>
+
+<div
+  class="rounded-b-4xl hero h-[60vh] bg-cover bg-bottom sm:bg-fixed sm:bg-center"
+  style="background-image: url(https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2);"
+>
+  <div class="hero-overlay bg-black/60 rounded-b-4xl"></div>
+  <div class="hero-content pt-24 text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="font-title mb-5 text-5xl font-bold">About Me</h1>
+      <p class="font-main mb-5 text-lg">
+	Learn more about me if you want to.
+      </p>
+    </div>
+  </div>
+</div>
+
+<main class="mx-auto max-w-4xl bg-base-100 px-4 py-16">
+  <div class="space-y-16">
+    <section id="deeper-dive">
+      <h2 class="font-title mb-6 text-center text-3xl font-semibold">Who art thou, Joshua?</h2>
+      <div class="prose prose-lg font-main max-w-none text-center">
+	<p>
+          Αγνή Παρθένε Δέσποινα, Άχραντε Θεοτόκε,<br />
+          Χαίρε Νύμφη Ανύμφευτε.<br />
+          Παρθένε Μήτηρ Άνασσα, Πανένδροσέ τε πόκε,<br />
+          Χαίρε Νύμφη Ανύμφευτε.
+        </p>
+        <p>
+          Υψηλοτέρα Ουρανών, ακτίνων λαμπροτέρα,<br />
+          Χαίρε Νύμφη Ανύμφευτε.<br />
+          Χαρά Παρθενικών Χορών, Αγγέλων υπερτέρα,<br />
+          Χαίρε Νύμφη Ανύμφευτε.
+        </p>
+        <p>
+          Εκλαμπροτέρα Ουρανών, φωτός καθαροτέρα,<br />
+          Χαίρε Νύμφη Ανύμφευτε.<br />
+          Των Ουρανίων στρατιών, πασών αγιωτέρα,<br />
+          Χαίρε Νύμφη Ανύμφευτε.
+        </p>
+      </div>
+    </section>
+
+    <section id="philosophy">
+      <h2 class="font-title mb-6 text-center text-3xl font-semibold">Where art thou going, Joshua?</h2>
+      <div class="font-main prose prose-lg max-w-none text-center">
+	<p>
+          Μαρία Αειπάρθενε, Κόσμου παντός Κυρία,<br />
+          Χαίρε Νύμφη Ανύμφευτε.<br />
+          Άχραντε Νύμφη Πάναγνε, Δέσποινα Παναγία,<br />
+          Χαίρε Νύμφη Ανύμφευτε.
+        </p>
+        <p>
+          Μαρία Νύμφη Άνασσα, χαράς ημών αιτία,<br />
+          Χαίρε Νύμφη Ανύμφευτε.<br />
+          Κορή σεμνή Βασίλισσα, Μήτηρ υπεραγία,<br />
+          Χαίρε Νύμφη Ανύμφευτε.
+        </p>
+        <p>
+          Τιμιωτέρα Χερουβείμ, υπερενδοξοτέρα,<br />
+          Χαίρε Νύμφη Ανύμφευτε.<br />
+          Των ασωμάτων Σεραφείμ, των Θρόνων ανωτέρα,<br />
+          Χαίρε Νύμφη Ανύμφευτε.
+        </p>
+      </div>
+    </section>
+  </div>
+</main>
