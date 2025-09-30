93fa2c69fd995e2e1098f3ce03cca0d36955136f
joshuafouch <joshfouch777@gmail.com>
2025-09-30 19:35:29 -0400

    hello

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -24,14 +24,14 @@
 
 <div
   class="rounded-b-4xl hero h-[60vh] bg-cover bg-bottom sm:bg-fixed sm:bg-center"
-  style="background-image: url(https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2);"
+  style="background-image: url(/assets/happy_snow.webp);"
 >
   <div class="hero-overlay bg-black/60 rounded-b-4xl"></div>
   <div class="hero-content pt-24 text-center text-neutral-content">
     <div class="max-w-md">
       <h1 class="font-title mb-5 text-5xl font-bold">About Me</h1>
-      <p class="font-main mb-5 text-lg">
-	Learn more about me if you want to.
+      <p class="font-main mb-5 text-lg text-red-800">
+        (Still in the works...)
       </p>
     </div>
   </div>
@@ -42,48 +42,16 @@
     <section id="deeper-dive">
       <h2 class="font-title mb-6 text-center text-3xl font-semibold">Who art thou, Joshua?</h2>
       <div class="prose prose-lg font-main max-w-none text-center">
-	<p>
-          Αγνή Παρθένε Δέσποινα, Άχραντε Θεοτόκε,<br />
-          Χαίρε Νύμφη Ανύμφευτε.<br />
-          Παρθένε Μήτηρ Άνασσα, Πανένδροσέ τε πόκε,<br />
-          Χαίρε Νύμφη Ανύμφευτε.
-        </p>
-        <p>
-          Υψηλοτέρα Ουρανών, ακτίνων λαμπροτέρα,<br />
-          Χαίρε Νύμφη Ανύμφευτε.<br />
-          Χαρά Παρθενικών Χορών, Αγγέλων υπερτέρα,<br />
-          Χαίρε Νύμφη Ανύμφευτε.
-        </p>
-        <p>
-          Εκλαμπροτέρα Ουρανών, φωτός καθαροτέρα,<br />
-          Χαίρε Νύμφη Ανύμφευτε.<br />
-          Των Ουρανίων στρατιών, πασών αγιωτέρα,<br />
-          Χαίρε Νύμφη Ανύμφευτε.
-        </p>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
       </div>
     </section>
 
-    <section id="philosophy">
+    <section id="more">
       <h2 class="font-title mb-6 text-center text-3xl font-semibold">Where art thou going, Joshua?</h2>
       <div class="font-main prose prose-lg max-w-none text-center">
-	<p>
-          Μαρία Αειπάρθενε, Κόσμου παντός Κυρία,<br />
-          Χαίρε Νύμφη Ανύμφευτε.<br />
-          Άχραντε Νύμφη Πάναγνε, Δέσποινα Παναγία,<br />
-          Χαίρε Νύμφη Ανύμφευτε.
-        </p>
-        <p>
-          Μαρία Νύμφη Άνασσα, χαράς ημών αιτία,<br />
-          Χαίρε Νύμφη Ανύμφευτε.<br />
-          Κορή σεμνή Βασίλισσα, Μήτηρ υπεραγία,<br />
-          Χαίρε Νύμφη Ανύμφευτε.
-        </p>
-        <p>
-          Τιμιωτέρα Χερουβείμ, υπερενδοξοτέρα,<br />
-          Χαίρε Νύμφη Ανύμφευτε.<br />
-          Των ασωμάτων Σεραφείμ, των Θρόνων ανωτέρα,<br />
-          Χαίρε Νύμφη Ανύμφευτε.
-        </p>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
       </div>
     </section>
   </div>
