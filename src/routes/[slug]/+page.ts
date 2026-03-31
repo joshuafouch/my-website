9caab858208e64f95610392f1bff7b3d8c8ae222
joshuafouch <joshfouch777@gmail.com>
2026-03-31 16:42:19 -0400

    Implement Project template.

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,14 +1,23 @@
-import { error } from '@sveltejs/kit'
+import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
-	try {
-		const post = await import(`../../posts/${params.slug}.md`)
+  try {
+    const post = await import(`../../posts/${params.slug}.md`);
 
-		return {
-			content: post.default,
-			meta: post.metadata
-		}
-	} catch (e) {
-		error(404, `Could not find ${params.slug}`)
-	}
+    return {
+      content: post.default,
+      meta: { ...post.metadata, type: 'post' }
+    };
+  } catch (e) {
+    try {
+      const project = await import(`../../projects/${params.slug}.md`);
+
+      return {
+        content: project.default,
+        meta: { ...project.metadata, type: 'project' }
+      };
+    } catch (e) {
+      error(404, `Could not find ${params.slug}`);
+    }
+  }
 }
