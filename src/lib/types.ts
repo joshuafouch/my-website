4be909335813d003cdc2b81330781915b9d8301f
joshuafouch <joshfouch777@gmail.com>
2026-03-31 16:42:45 -0400

    Create Project Type.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,22 @@
-export type Categories = 'sveltekit' | 'svelte'
+export type Categories = 'sveltekit' | 'svelte';
 
 export type Post = {
-	title: string
-	slug: string
-	description: string
-	date: string
-	categories: Categories[]
-	image: string
-	published: boolean
-}
+  title: string;
+  slug: string;
+  description: string;
+  date: string;
+  categories: Categories[];
+  image: string;
+  published: boolean;
+};
+
+export type Project = {
+  title: string;
+  slug: string;
+  description: string;
+  date: string;
+  image: string;
+  sourceUrl?: string;
+  tags?: string[];
+  published: boolean;
+};
